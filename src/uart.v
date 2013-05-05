e41e756970872802da7f80dd5b50055ad6f6b64b
MartinRuan <632303445@qq.com>
2013-05-05 13:08:31 +0800

    实现发送功能

--- src/uart.v
+++ src/uart.v
@@ -24,9 +24,13 @@ reg rx_dly;
 reg [13:0] rx_cnt;
 reg [3:0] data_cnt;
 reg data_vld;
-wire rx_change, rx_en;
+wire rx_change;
+wire rx_en;
 
 reg [7:0] tx_rdy_data;
+reg tran_vld;
+reg [3: 0] tran_cnt;
+reg tx;
 
 always @(posedge clk) begin
 	rx1 <= rx;
@@ -108,4 +112,60 @@ always @(posedge clk or posedge rst) begin
 	else;
 end
 
+always @(posedge clk or posedge rst) being
+	if (rst) begin
+        tran_vld <= 1'b0;		
+	end
+	else if (tx_vld) begin
+        tran_vld <= 1'b1;
+	end 
+	else if (tran_vld & rx_en &(tran_cnt == 4'b10)) begin
+        tran_vld <= 1'b0;
+	end
+	else;
+end
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        tran_cnt <= 4'b0;
+    end
+    else if (tran_vld) begin
+        if (rx_en) begin
+            tran_cnt <= tran_cnt + 1;
+        end
+        else;
+    end
+    else begin
+        tran_cnt <= 4'b0;
+    end
+end
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        tx <= 1'b1;
+    end 
+    else if (tran_vld) begin
+        if (rx_en) begin
+            case (tran_cnt)
+                4'd0: tx <= 1'b0;
+                4'd1: tx <= tx_rdy_data[0];
+                4'd2: tx <= tx_rdy_data[1];
+                4'd3: tx <= tx_rdy_data[2];
+                4'd4: tx <= tx_rdy_data[3];
+                4'd5: tx <= tx_rdy_data[4];
+                4'd6: tx <= tx_rdy_data[5];
+                4'd7: tx <= tx_rdy_data[6];
+                4'd8: tx <= tx_rdy_data[7];
+                4'd9: tx <= ^tx_rdy_data;
+                4'd10 tx <= 1'b1;
+                default: tx <= 1'b1;
+            endcase
+        end
+    end
+    else begin
+        tx <= 1'b1;
+    end
+end
+
+assign txrdy = ~tran_vld;
 endmodule
